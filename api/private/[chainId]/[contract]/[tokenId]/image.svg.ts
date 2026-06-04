f0a2c69b76ebd3db05b80ecddeb4929ff7d76ff2
HardlyDifficult <hardlydiff@gmail.com>
2026-06-04 18:47:48 -0400

    Fix Vercel private image URLs

--- api/private/[chainId]/[contract]/[tokenId]/image.svg.ts
+++ api/private/[chainId]/[contract]/[tokenId]/image.svg.ts
@@ -2,13 +2,12 @@ import { verifyProtectedRequest } from "../../../../lib/protected-resource.js";
 import {
   optionalParam,
   requiredParam,
-  requestUrl,
   text,
   type ApiRequest,
   type ApiResponse,
 } from "../../../../lib/http.js";
 import { verifyResourceToken } from "../../../../lib/resource-token.js";
-import { routeResource } from "../../../../lib/resources.js";
+import { privateImageUrl, routeResource } from "../../../../lib/resources.js";
 import { privateSvg } from "../../../../lib/svg.js";
 
 export default async function handler(
@@ -20,7 +19,7 @@ export default async function handler(
     contract: requiredParam(req, "contract"),
     tokenId: requiredParam(req, "tokenId"),
   });
-  const unsignedResourceUri = unsignedRequestUrl(req);
+  const unsignedResourceUri = privateImageUrl(req, route);
   if (
     verifyResourceToken(optionalParam(req, "access_token"), unsignedResourceUri)
   ) {
@@ -37,10 +36,4 @@ export default async function handler(
   if (!authorization) return;
 
   text(res, 200, privateSvg(route.tokenId), "image/svg+xml; charset=utf-8");
-}
-
-function unsignedRequestUrl(req: ApiRequest): string {
-  const url = new URL(requestUrl(req));
-  url.searchParams.delete("access_token");
-  return url.toString();
 }
